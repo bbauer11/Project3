1d0c622b13688f875ba6b50511daf2a67a45dfaa
Brian Bauer <60554854+bbauer11@users.noreply.github.com>
2020-03-08 21:01:03 -0400

    commit0.1

---
+++ BasicDoubleLinkedList.java
@@ -0,0 +1,223 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+/**
+ * 
+ * @author Brian Bauer
+ * 
+ *
+ * @param <T>
+ */
+public class BasicDoubleLinkedList<T> implements Iterable<T> {
+	
+	Node head;
+	Node tail;
+	int size;
+
+	public BasicDoubleLinkedList() {
+		head = null;
+		tail = null;
+		size = 0;
+	}
+	
+	public BasicDoubleLinkedList<T> addToFront(T data) {
+		if (size == 0) {
+			head = new Node(data);
+			tail = head;
+		} else {
+			Node temp = new Node(data);
+			head.prev = temp;
+			temp.next = head;
+			head = temp;
+		}
+		size++;
+		return this;
+	}
+	
+	public BasicDoubleLinkedList<T> addToEnd(T data) {
+		if (size == 0) {
+			head = new Node(data);
+			tail = head;
+		} else {
+			Node temp = new Node(data);
+			temp.prev = tail;
+			tail.next = temp;
+			tail = temp;
+		}
+		size++;
+		return this;
+	}
+	
+	public T getFirst() {
+		return head.data;
+	}
+	
+	public T getLast() {
+		return tail.data;
+	}
+	
+	public ListIterator<T> iterator() {
+		return new MyIterator<T>();
+	}
+
+	public ArrayList<T> toArrayList() {
+		ArrayList<T> arrayList = new ArrayList<>();
+		Node curr = head;
+		while (curr != tail) {
+			arrayList.add(curr.data);
+			curr = curr.next;
+		}
+		arrayList.add(curr.data);
+		return arrayList;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public T retrieveLastElement() {
+		if (size == 0)
+			return null;
+		else if (size == 1) {
+			T data = tail.data;
+			size--;
+			tail = null;
+			head = null;
+			return data;
+		} else if (size == 2) {
+			T data = tail.data;
+			size--;
+			head.next = null;
+			tail = head;
+			return data;
+		} else {
+			T data = tail.data;
+			tail.prev.next = null;
+			tail = tail.prev;			
+			size--;
+			return data;
+		}	
+	}
+	
+	public T retrieveFirstElement() {
+		if (size == 0)
+			return null;
+		else if (size == 1) {
+			T data = head.data;
+			head = null;
+			tail = null;
+			size--;
+			return data;
+		} else if (size == 2) {
+			T data = head.data;
+			size--;
+			tail.prev = null;
+			head = tail;
+			return data;
+		} else {
+			T data = head.data;
+			head.next.prev = null;
+			head = head.next;
+			size--;
+			return data;
+		}
+	}
+	
+	public BasicDoubleLinkedList<T> remove(T data, Comparator<T> comparator) {
+		Node temp = new Node(data);
+		Node curr = head;
+		while (comparator.compare(curr.data, temp.data) != 0 && curr.next != null) {
+			curr = curr.next;
+		}
+		if (curr.next == null) 
+			return null;
+		else {
+			curr.next.prev = curr.prev;
+			curr.prev.next = curr.next;
+			return this;
+		}
+	}
+	
+	class MyIterator<T> implements ListIterator<T> {
+
+		private Node nextNode;
+		
+		private MyIterator() {
+			nextNode = head;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return nextNode.next == null ?  false : true;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public T next() {
+			if (hasNext()) {
+				Node temp = nextNode;
+				nextNode = nextNode.next;
+				return (T) temp.data;
+			} else 
+				throw new NoSuchElementException("Illegal call to next(); iterator is after end of list.");
+		}
+
+		@Override
+		public boolean hasPrevious() {
+			return nextNode.prev != null ? true : false;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public T previous() {
+			if (hasPrevious()) {
+				Node temp = nextNode;
+				nextNode = nextNode.prev;
+				return (T) temp.data;
+			}
+			throw new NoSuchElementException();
+		}
+
+		@Override
+		public int nextIndex() {
+			throw new UnsupportedOperationException("Method not implemented.");
+		}
+
+		@Override
+		public int previousIndex() {
+			throw new UnsupportedOperationException("Method not implemented.");
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException("Method not implemented.");
+		}
+
+		@Override
+		public void set(T e) {
+			throw new UnsupportedOperationException("Method not implemented.");
+		}
+
+		@Override
+		public void add(T e) {
+			throw new UnsupportedOperationException("Method not implemented.");
+		}
+		
+	}
+	
+	class Node {
+		T data;
+		Node next;
+		Node prev;
+		
+		public Node(T data) {
+			this.data = data;
+			next = null;
+			prev = null;
+		}	
+	}
+
+
+}
---
+++ SortedDoubleLinkedList.java
@@ -0,0 +1,80 @@
+import java.util.Comparator;
+import java.util.ListIterator;
+
+/**
+ * 
+ * @author Brian Bauer
+ *
+ * @param <T>
+ */
+public class SortedDoubleLinkedList<T> extends BasicDoubleLinkedList<T> {
+
+	private Comparator<T> comparator;
+	
+	public SortedDoubleLinkedList(Comparator<T> comparator2) {
+		head = null;
+		tail = null;
+		size = 0;
+		this.comparator = comparator2;
+	}
+
+	@Override
+	public BasicDoubleLinkedList<T> addToFront(T data) {
+		throw new UnsupportedOperationException("Method not supported for sorted list.");
+	}
+	
+	@Override
+	public BasicDoubleLinkedList<T> addToEnd(T data) {
+		throw new UnsupportedOperationException("Method not supported for sorted list.");
+	}
+
+	public SortedDoubleLinkedList<T> add(T data) {
+		if (size == 0) {
+			head = new Node(data);
+			tail = head;
+			size++;
+		} else {
+			Node temp = new Node(data);
+			Node curr = head;
+			while (comparator.compare(curr.data, temp.data) < 0 && curr.next != null) {
+				curr = curr.next;
+			}
+			if (curr.next == null) {
+				tail.next = temp;
+				temp.prev = tail;
+				tail = temp;
+				return this;
+			} else if (comparator.compare(curr.data, temp.data) == 0) {
+				if (curr == head) {
+					temp.next = curr.next;
+					temp.prev = curr;
+					temp.next.prev = temp;
+					curr.next = temp;
+					return this;
+				} else {
+					temp.next = curr;
+					curr.prev.next = temp;
+					temp.prev = curr.prev;
+					curr.prev = temp;
+					return this;
+				}
+			} else {
+				temp.next = curr.next;
+				temp.prev = curr;
+				curr.next.prev = temp;
+				curr.next = temp;
+				return this;
+			}
+		} 
+		return this;
+	}
+	
+	public ListIterator<T> iterator() {
+		return super.iterator();
+	}
+	
+	public SortedDoubleLinkedList<T> remove(T data, Comparator<T> comparator) {
+		super.remove(data, comparator);
+		return this;
+	}
+}
